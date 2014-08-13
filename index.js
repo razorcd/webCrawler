296337faa73a36aa808dd2ea4a9694df6f3c01fb
Razor <razorcd@yahoo.com>
2014-08-14 01:53:39 +0300

    port changed

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ app.get('/crawl', function(req,res){
 
 
 
-
-app.listen(8080);
-console.log('server started on port: 8080');
+var port = Number(process.env.PORT || 5000);
+app.listen(port);
+console.log('server started on port: ' + port);
